d9f71999b3104b833521dad431572d80f69b4c1e
J. Jesús González Mairena <jiuxej@gmail.com>
2022-04-22 21:58:45 +0200

    Prueba de lectura de MongoDB

--- scripts/index.py
+++ scripts/index.py
@@ -8,9 +8,19 @@ TIMEOUT = 1000
 
 MONGO_URI = "mongodb://" + MONGO_HOST + ":" + MONGO_PORT + "/"
 
+MONGO_DB = "escuela"
+MONGO_DB_COLLECTION = "alumnos"
+
 try:
     client = pymongo.MongoClient(MONGO_URI, serverSelectionTimeoutMS=TIMEOUT)
     print("Connected to MongoDB")
+
+    bd = client[MONGO_DB]
+    collection = bd[MONGO_DB_COLLECTION]
+
+    for doc in collection.find():
+        print("Nombre: " + doc['nombre'] + " Sexo: " + doc['sexo'] + " Calificacion: " + str(doc['calificacion']))
+
     client.close()
 except pymongo.errors.ServerSelectionTimeoutErrror as e:
     print("Timeout error: " + str(e))
